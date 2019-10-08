80af9a6fed9487cc6e7bb595616936797aa62425
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-10-08 13:15:43 +0200

    src: minor cleanup to signals.c

--- signals.c
+++ signals.c
@@ -4,8 +4,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
-
 void handleSignal(int signo) {
     if (signo == SIGUSR1) {
         printf("SIGURS1. Just printing. Carry on\n");
@@ -15,21 +13,18 @@ void handleSignal(int signo) {
     }
 }
 
-// kill -USR1 16932
-//
-// void (*signal(int sig, void (*func)(int)))(int);
-// So what we have is a function pointer, signal which takes an int
-// and a function pointer of that takes an int and returns nothing.
-// The signal function itself returns an a function pointer to the
-// previous handler (if any). And a handler returns void and takes an
-// int which explains the first void and the last (int)
-//
 int main(int argc, char **argv) {
     printf("pid: %d\n", getpid());
-    void (*old)(int);
-    old = signal(SIGUSR1, handleSignal);
-    signal(SIGUSR2, handleSignal);
+    if (signal(SIGUSR1, handleSignal) == SIG_ERR) {
+      fprintf(stderr, "Cannot handle SIGUSR1 \n");
+      exit(EXIT_FAILURE);
+    }
+    if (signal(SIGUSR2, handleSignal) == SIG_ERR) {
+      fprintf(stderr, "Cannot handle SIGUSR2 \n");
+      exit(EXIT_FAILURE);
+    }
     for (;;) {
+      pause();
     }
     return 0;
 }
