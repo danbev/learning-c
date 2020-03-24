c75c1295cb34b3af7609356a551d1b60370a21c5
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-03-24 07:50:37 +0100

    cmake-example: add shared library example

---
+++ cmake-example/sharedlib/src/something.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void shared_something() {
+  printf("sharedlib/something...\n");
+}
--- cmake-example/src/hello.c
+++ cmake-example/src/hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "something.h"
+#include "sharedsomething.h"
 
 int main(int argc, char** argv) {
   printf("cmake example...\n");
   something();
+  shared_something();
   return 0;
 }
