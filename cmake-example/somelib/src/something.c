ed8a54b9843f59656781cea52611249a53f28f04
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-03-24 07:28:03 +0100

    cmake-example: add library example

---
+++ cmake-example/somelib/src/something.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void something() {
+  printf("something...\n");
+}
--- cmake-example/src/hello.c
+++ cmake-example/src/hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "something.h"
 
 int main(int argc, char** argv) {
   printf("cmake example...\n");
+  something();
   return 0;
 }
